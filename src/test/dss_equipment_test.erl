90d1b292d866f395c4001980c1ac8cef9ea4236c
UlangBoney <ulang.2106@gmail.com>
2019-05-12 01:21:41 +0900

    add straight sword test

--- src/test/dss_equipment_test.erl
+++ src/test/dss_equipment_test.erl
@@ -5,12 +5,14 @@
 
 -spec lookup_test() -> ok.
 lookup_test() ->
-     ok = dagger_lookup_test().
+     ok = dagger_lookup_test(),
+     ok = straight_sword_lookup_test().
 
 
 -spec list_test() -> ok.
 list_test() ->
-     ok = dagger_list_test().
+     ok = dagger_list_test(),
+     ok = straight_sword_list_test().
 
 
 -spec dagger_lookup_test() -> ok.
@@ -25,10 +27,49 @@ dagger_lookup_test() ->
     ).
 
 
+-spec straight_sword_lookup_test() -> ok.
+straight_sword_lookup_test() ->
+    none = dss_equipment:lookup(6, straight_sword),
+    none = dss_equipment:lookup(20, straight_sword),
+    ok = lists:foreach(
+        fun(EquipmentID) ->
+            straight_sword_lookup_test(EquipmentID)
+        end,
+        lists:seq(7, 19)
+    ).
+
+
+-spec weapon_lookup_test(
+        dss_equipment:id(),
+        dss_equipment:equipment_type(),
+        unicode:unicode_binary(),
+        unicode:unicode_binary(),
+        float(),
+        dss_equipment:requirement()
+    ) -> true.
+weapon_lookup_test(EquipmentID, EqpType, Ename, Jname, Weigth, #{strenght := Strength, dexterity := Dexterity, intelligence := Intelligence, faith := Faith}) ->
+    {value, Equipment} = dss_equipment:lookup(EqpType, EquipmentID),
+    EquipmentID  = maps:get(id, Equipment),
+    EName        = maps:get(english, maps:get(name, Equipment)),
+    JName        = maps:get(japanese, maps:get(name, Equipment)),
+    Weight       = maps:get(weight, Equipment),
+    Strength     = maps:get(strength, maps:get(requirements, Equipment)),
+    Dexterity    = maps:get(dexterity, maps:get(requirements, Equipment)),
+    Intelligence = maps:get(intelligence, maps:get(requirements, Equipment)),
+    Faith        = maps:get(faith, maps:get(requirements, Equipment)).
+
+
 -spec dagger_list_test() -> ok.
 dagger_list_test() ->
     DaggerList = dss_equipment:list(dagger),
     6 = length(DaggerList),
+    ok.
+
+
+-spec straight_sword_list_test() -> ok.
+straight_sword_list_test() ->
+    StraightSwordList = dss_equipment:list(straight_sword),
+    12 = length(StraightSwordList),
     ok.
 
 
@@ -71,22 +112,83 @@ dagger_lookup_test(6) ->
     weapon_lookup_test(6, dagger, <<"priscilla's dagger">>, <<"プリシラの短剣">>, 1, Requirements).
 
 
--spec weapon_lookup_test(
-        dss_equipment:id(),
-        dss_equipment:equipment_type(),
-        unicode:unicode_binary(),
-        unicode:unicode_binary(),
-        float(),
-        dss_equipment:requirement()
-    ) -> true.
-weapon_lookup_test(EquipmentID, EqpType, Ename, Jname, Weigth, #{strenght := Strength, dexterity := Dexterity, intelligence := Intelligence, faith := Faith}) ->
-    {value, Equipment} = dss_equipment:lookup(EqpType, EquipmentID),
-    EquipmentID  = maps:get(id, Equipment),
-    EName        = maps:get(english, maps:get(name, Equipment)),
-    JName        = maps:get(japanese, maps:get(name, Equipment)),
-    Weight       = maps:get(weight, Equipment),
-    Strength     = maps:get(strength, maps:get(requirements, Equipment)),
-    Dexterity    = maps:get(dexterity, maps:get(requirements, Equipment)),
-    Intelligence = maps:get(intelligence, maps:get(requirements, Equipment)),
-    Faith        = maps:get(faith, maps:get(requirements, Equipment)).
+-spec straight_sword_lookup_test(pos_integer()) -> pos_integer().
+straight_sword_lookup_test(7) ->
+    Requirements = #{ strenght     => 8
+                    , dexterity    => 10
+                    , intelligence => 0
+                    , faith        => 0},
+    weapon_lookup_test(7, straight_sword, <<"short sword">>, <<"ショートソード">>, 2, Requirements);
+straight_sword_lookup_test(8) ->
+    Requirements = #{ strenght     => 10
+                    , dexterity    => 10
+                    , intelligence => 0
+                    , faith        => 0},
+    weapon_lookup_test(8, straight_sword, <<"long sword">>, <<"ロングソード">>, 2, Requirements);
+straight_sword_lookup_test(9) ->
+    Requirements = #{ strenght     => 10
+                    , dexterity    => 10
+                    , intelligence => 0
+                    , faith        => 0},
+    weapon_lookup_test(9, straight_sword, <<"broad sword">>, <<"ブロードソード">>, 3, Requirements);
+straight_sword_lookup_test(10) ->
+    Requirements = #{ strenght     => 10
+                    , dexterity    => 14
+                    , intelligence => 0
+                    , faith        => 0},
+    weapon_lookup_test(10, straight_sword, <<"balder side sword">>, <<"バルデルの刺突直剣">>, 3, Requirements);
+straight_sword_lookup_test(11) ->
+    Requirements = #{ strenght     => 12
+                    , dexterity    => 12
+                    , intelligence => 0
+                    , faith        => 0},
+    weapon_lookup_test(11, straight_sword, <<"sunlight straight sword">>, <<"太陽の直剣">>, 4, Requirements);
+straight_sword_lookup_test(12) ->
+    Requirements = #{ strenght     => 16
+                    , dexterity    => 16
+                    , intelligence => 0
+                    , faith        => 0},
+    weapon_lookup_test(12, straight_sword, <<"dark sword">>, <<"ダークソード">>, 6, Requirements);
+straight_sword_lookup_test(13) ->
+    Requirements = #{ strenght     => 10
+                    , dexterity    => 10
+                    , intelligence => 0
+                    , faith        => 0},
+    weapon_lookup_test(13, straight_sword, <<"barbed straight sword">>, <<"トゲの直剣">>, 3, Requirements);
+straight_sword_lookup_test(14) ->
+    Requirements = #{ strenght     => 16
+                    , dexterity    => 10
+                    , intelligence => 0
+                    , faith        => 0},
+    weapon_lookup_test(14, straight_sword, <<"crystal straight sword">>, <<"結晶直剣">>, 6, Requirements);
+straight_sword_lookup_test(15) ->
+    Requirements = #{ strenght     => 16
+                    , dexterity    => 22
+                    , intelligence => 0
+                    , faith        => 0},
+    weapon_lookup_test(15, straight_sword, <<"silver knight straight sword">>, <<"銀騎士の剣">>, 6, Requirements);
+straight_sword_lookup_test(16) ->
+    Requirements = #{ strenght     => 10
+                    , dexterity    => 10
+                    , intelligence => 0
+                    , faith        => 14},
+    weapon_lookup_test(16, straight_sword, <<"astora's straight sword">>, <<"アストラの直剣">>, 3, Requirements);
+straight_sword_lookup_test(17) ->
+    Requirements = #{ strenght     => 16
+                    , dexterity    => 10
+                    , intelligence => 0
+                    , faith        => 0},
+    weapon_lookup_test(17, straight_sword, <<"drake sword">>, <<"飛竜の剣">>, 6, Requirements);
+straight_sword_lookup_test(18) ->
+    Requirements = #{ strenght     => 8
+                    , dexterity    => 8
+                    , intelligence => 0
+                    , faith        => 0},
+    weapon_lookup_test(18, straight_sword, <<"broken straight sword">>, <<"折れた直剣">>, 2, Requirements);
+straight_sword_lookup_test(19) ->
+    Requirements = #{ strenght     => 6
+                    , dexterity    => 6
+                    , intelligence => 0
+                    , faith        => 0},
+    weapon_lookup_test(19, straight_sword, <<"straight sword hilt">>, <<"直剣の柄">>, 1, Requirements).
 
