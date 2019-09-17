405e05a8ca8bff21fc8317ee8d43d7b7a8b5fd57
UlangBoney <ulang.2106@gmail.com>
2019-09-17 23:21:14 +0900

    Fix: classes test to utf8

--- test/dss_classes_SUITE.erl
+++ test/dss_classes_SUITE.erl
@@ -46,25 +46,25 @@ list_test() ->
 lookup_test(0) ->
     none = dss_classes:lookup(0);
 lookup_test(1) ->
-    lookup_test(1, <<"warrior">>, <<"戦士">>, 4, 11, 8, 12, 13, 13, 11, 9, 9, 0);
+    lookup_test(1, <<"warrior">>, <<"戦士"/utf8>>, 4, 11, 8, 12, 13, 13, 11, 9, 9, 0);
 lookup_test(2) ->
-    lookup_test(2, <<"knight">>, <<"騎士">>, 5, 14, 10, 10, 11, 11, 10, 9, 11, 1);
+    lookup_test(2, <<"knight">>, <<"騎士"/utf8>>, 5, 14, 10, 10, 11, 11, 10, 9, 11, 1);
 lookup_test(3) ->
-    lookup_test(3, <<"wanderer">>, <<"放浪者">>, 3, 10, 11, 10, 10, 14, 12, 11, 8, 1);
+    lookup_test(3, <<"wanderer">>, <<"放浪者"/utf8>>, 3, 10, 11, 10, 10, 14, 12, 11, 8, 1);
 lookup_test(4) ->
-    lookup_test(4, <<"thief">>, <<"盗人">>, 5, 9, 11, 9, 9, 15, 10, 12, 11, 1);
+    lookup_test(4, <<"thief">>, <<"盗人"/utf8>>, 5, 9, 11, 9, 9, 15, 10, 12, 11, 1);
 lookup_test(5) ->
-    lookup_test(5, <<"bandit">>, <<"山賊">>, 4, 12, 8, 14, 14, 9, 11, 8, 10, 0);
+    lookup_test(5, <<"bandit">>, <<"山賊"/utf8>>, 4, 12, 8, 14, 14, 9, 11, 8, 10, 0);
 lookup_test(6) ->
-    lookup_test(6, <<"hunter">>, <<"狩人">>, 4, 11, 9, 11, 12, 14, 11, 9, 9, 0);
+    lookup_test(6, <<"hunter">>, <<"狩人"/utf8>>, 4, 11, 9, 11, 12, 14, 11, 9, 9, 0);
 lookup_test(7) ->
-    lookup_test(7, <<"sorcerer">>, <<"魔術師">>, 3, 8, 15, 8, 9, 11, 8, 15, 8, 3);
+    lookup_test(7, <<"sorcerer">>, <<"魔術師"/utf8>>, 3, 8, 15, 8, 9, 11, 8, 15, 8, 3);
 lookup_test(8) ->
-    lookup_test(8, <<"pyromancer">>, <<"呪術師">>, 1, 10, 12, 11, 12, 9, 12, 10, 8, 2);
+    lookup_test(8, <<"pyromancer">>, <<"呪術師"/utf8>>, 1, 10, 12, 11, 12, 9, 12, 10, 8, 2);
 lookup_test(9) ->
-    lookup_test(9, <<"cleric">>, <<"聖職者">>, 2, 11, 11, 9, 12, 8, 11, 8, 14, 1);
+    lookup_test(9, <<"cleric">>, <<"聖職者"/utf8>>, 2, 11, 11, 9, 12, 8, 11, 8, 14, 1);
 lookup_test(10) ->
-    lookup_test(10, <<"deprived">>, <<"持たざるもの">>, 6, 11, 11, 11, 11, 11, 11, 11, 11, 1);
+    lookup_test(10, <<"deprived">>, <<"持たざるもの"/utf8>>, 6, 11, 11, 11, 11, 11, 11, 11, 11, 1);
 lookup_test(11) ->
     none = dss_classes:lookup(11).
 
@@ -84,12 +84,12 @@ lookup_test(11) ->
         pos_integer(),
         pos_integer()
     ) -> pos_integer().
-lookup_test(ClassID, Ename, _Jname, Levels, Vitality, Attunement, Endurance
+lookup_test(ClassID, Ename, Jname, Levels, Vitality, Attunement, Endurance
           , Strength, Dexterity, Resistance, Intelligence, Faith, AttSlots) ->
     {value, Class} = dss_classes:lookup(ClassID),
     ClassID        = maps:get(id, Class),
     Ename          = maps:get(english, maps:get(name, Class)),
-    _              = maps:get(japanese, maps:get(name, Class)),
+    Jname          = maps:get(japanese, maps:get(name, Class)),
     Levels         = maps:get(levels, Class),
     Vitality       = maps:get(vitality, Class),
     Attunement     = maps:get(attunement, Class),
